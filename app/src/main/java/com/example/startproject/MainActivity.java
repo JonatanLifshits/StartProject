337695cd02c2edce6102299e25a544769c4547db
Jon <djonli@mail.ua>
2018-06-08 13:03:34 +0300

    - reiteration calculator

--- app/src/main/java/com/example/startproject/MainActivity.java
+++ app/src/main/java/com/example/startproject/MainActivity.java
@@ -2,16 +2,29 @@ package com.example.startproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    private TextView tvOut;
-    private Button btnOk;
-    private Button btnCancel;
+    final int MENU_RESET_ID = 1;
+    final int MENU_QUIT_ID = 2;
+
+    private EditText etNum1;
+    private EditText etNum2;
+
+    private Button btnAdd;
+    private Button btnSub;
+    private Button btnMult;
+    private Button btnDiv;
+
+    private TextView tvResult;
+    String oper = " ";
+
 
     public static final String TAG = "My logs";
 
@@ -19,31 +32,54 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG,"найдем view элементы");
-        tvOut = findViewById(R.id.tvOut);
-        btnOk = findViewById(R.id.btnOk);
-        btnCancel = findViewById(R.id.btnCancel);
+        etNum1 = findViewById(R.id.etNum1);
+        etNum2 = findViewById(R.id.etNum2);
 
-        Log.d(TAG,"присваеваем обработчик кнопкам");
-        btnOk.setOnClickListener(this);
-        btnCancel.setOnClickListener(this);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnMult = findViewById(R.id.btnMult);
+        btnSub = findViewById(R.id.btnSub);
+
+        tvResult = findViewById(R.id.tvResult);
+
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Log.d(TAG ,"по id определяем кнопку, вызвавшую этот обработчик");
-       switch(v.getId()){
-           case R.id.btnOk:
-               Log.d(TAG ,"кнопка ОК");
-               tvOut.setText("Нажата кнопка Ок");
-               Toast.makeText(this, "нажата кнопка Ок" , Toast.LENGTH_LONG).show();
-               break;
-           case R.id.btnCancel:
-               Log.d(TAG ,"кнопка Cancel");
-               tvOut.setText("Нажата кнопка Cancel");
-               Toast.makeText(this ,"нажата кнопка Cancel" ,Toast.LENGTH_LONG).show();
-               break;
-       }
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+        }
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
     }
 }
-
