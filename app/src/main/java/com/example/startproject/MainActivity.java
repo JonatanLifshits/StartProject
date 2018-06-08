76d7316a8d8e3fe2e516428a508ccf83ecdc876c
Jon <djonli@mail.ua>
2018-06-08 13:16:48 +0300

    - reiteration browser

--- app/src/main/java/com/example/startproject/MainActivity.java
+++ app/src/main/java/com/example/startproject/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity {
     final int MENU_RESET_ID = 1;
     final int MENU_QUIT_ID = 2;
 
@@ -32,54 +32,5 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNum1 = findViewById(R.id.etNum1);
-        etNum2 = findViewById(R.id.etNum2);
-
-        btnAdd = findViewById(R.id.btnAdd);
-        btnDiv = findViewById(R.id.btnDiv);
-        btnMult = findViewById(R.id.btnMult);
-        btnSub = findViewById(R.id.btnSub);
-
-        tvResult = findViewById(R.id.tvResult);
-
-        btnSub.setOnClickListener(this);
-        btnMult.setOnClickListener(this);
-        btnDiv.setOnClickListener(this);
-        btnAdd.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        float num1 = 0;
-        float num2 = 0;
-        float result = 0;
-
-        if (TextUtils.isEmpty(etNum1.getText().toString())
-                || TextUtils.isEmpty(etNum2.getText().toString())) {
-            return;
-        }
-        num1 = Float.parseFloat(etNum1.getText().toString());
-        num2 = Float.parseFloat(etNum2.getText().toString());
-        switch (v.getId()) {
-            case R.id.btnAdd:
-                oper = "+";
-                result = num1 + num2;
-                break;
-            case R.id.btnSub:
-                oper = "-";
-                result = num1 - num2;
-                break;
-            case R.id.btnMult:
-                oper = "*";
-                result = num1 * num2;
-                break;
-            case R.id.btnDiv:
-                oper = "/";
-                result = num1 / num2;
-                break;
-            default:
-                break;
-        }
-        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
     }
 }
