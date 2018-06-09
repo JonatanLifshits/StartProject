2c075935a4988a434b892f34aecbca07fe51d6ad
Jon <djonli@mail.ua>
2018-06-09 14:28:44 +0300

    - reiteration TimePicker Dialog

--- app/src/main/java/com/example/startproject/MainActivity.java
+++ app/src/main/java/com/example/startproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.startproject;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -8,29 +10,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    final int MENU_RESET_ID = 1;
-    final int MENU_QUIT_ID = 2;
-
-    private EditText etNum1;
-    private EditText etNum2;
-
-    private Button btnAdd;
-    private Button btnSub;
-    private Button btnMult;
-    private Button btnDiv;
-
-    private TextView tvResult;
-    String oper = " ";
-
-
-    public static final String TAG = "My logs";
+    int DIALOG_TIME = 1;
+    int myHours = 14;
+    int myMinutes = 35;
+    private TextView tvTime;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvTime = findViewById(R.id.tvTime);
     }
+    public void onclick(View v){
+        showDialog(DIALOG_TIME);
+    }
+    protected Dialog onCreateDialog(int id){
+        if (id == DIALOG_TIME){
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHours, myMinutes, true);
+            return tpd;
+        }
+        return super.onCreateDialog(id);
+    }
+    TimePickerDialog.OnTimeSetListener myCallBack = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHours = hourOfDay;
+            myMinutes = minute;
+            tvTime.setText("Time is " + myHours + " hours " + myMinutes + " minutes");
+        }
+    };
 }
