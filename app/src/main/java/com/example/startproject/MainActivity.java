5f7631933ee37de6f7486a5a064fb761d361fcc1
Jon <djonli@mail.ua>
2018-06-09 15:00:53 +0300

    - reiteration SharedPreferences

--- app/src/main/java/com/example/startproject/MainActivity.java
+++ app/src/main/java/com/example/startproject/MainActivity.java
@@ -2,30 +2,59 @@ package com.example.startproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity implements View.OnClickListener {
-     EditText etFName;
-     EditText etLName;
+    private EditText etText;
+    private Button btnSave, btnLoad;
 
-    private Button btnSumbit;
+    SharedPreferences sPref;
+
+    final String SAVED_TEXT = "saved_text";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        etFName = findViewById(R.id.etFName);
-        etLName = findViewById(R.id.etLName);
+        loadText();
 
-        btnSumbit = findViewById(R.id.btnSubmit);
-        btnSumbit.setOnClickListener(this);
+        setContentView(R.layout.activity_main);
+        etText = findViewById(R.id.etText);
+
+        btnSave = findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+
+        btnLoad = findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(this, ViewActivity.class);
+        switch (v.getId()){
+            case R.id.btnSave:
+                saveText();
+                break;
+                default:
+                    break;
+
+        }
+    }
+    void saveText(){
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed  = sPref.edit();
+        ed.putString(SAVED_TEXT, etText.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+
+    }
+    void loadText(){
+        sPref = getPreferences(MODE_PRIVATE);
+        String savedText = sPref.getString(SAVED_TEXT, "");
+        etText.setText(savedText);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
     }
 }
