505cc3ce05c2013926e969398fe1c558ab443bcb
Jon <djonli@mail.ua>
2018-06-10 10:16:38 +0300

    - reiteration BroadcastReceiver

--- app/src/main/java/com/example/startproject/MainActivity.java
+++ app/src/main/java/com/example/startproject/MainActivity.java
@@ -2,59 +2,34 @@ package com.example.startproject;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 
 public class MainActivity extends Activity implements View.OnClickListener {
-    private EditText etText;
-    private Button btnSave, btnLoad;
-
-    SharedPreferences sPref;
-
-    final String SAVED_TEXT = "saved_text";
+    public static final String NEW_CAT_DETECTED = "ru.alexanderklimov.action.NEW_CAT";
+    public static final String ALARM_MESSAGE = "Срочно пришлите кота";
+    private Button btnSM;
+    byte CatName;
+    byte currentLongitude;
+    byte currentLatitude;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        loadText();
-
         setContentView(R.layout.activity_main);
-        etText = findViewById(R.id.etText);
+        btnSM = findViewById(R.id.btnSM);
+        btnSM.setOnClickListener(this);
 
-        btnSave = findViewById(R.id.btnSave);
-        btnSave.setOnClickListener(this);
-
-        btnLoad = findViewById(R.id.btnLoad);
-        btnLoad.setOnClickListener(this);
+        Intent intent = new Intent(NEW_CAT_DETECTED);
+        intent.putExtra("cat name", CatName);
+        intent.putExtra("longitude", currentLongitude);
+        intent.putExtra("latitude", currentLatitude);
+        sendBroadcast(intent);
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btnSave:
-                saveText();
-                break;
-                default:
-                    break;
 
-        }
-    }
-    void saveText(){
-        sPref = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor ed  = sPref.edit();
-        ed.putString(SAVED_TEXT, etText.getText().toString());
-        ed.commit();
-        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
-
-    }
-    void loadText(){
-        sPref = getPreferences(MODE_PRIVATE);
-        String savedText = sPref.getString(SAVED_TEXT, "");
-        etText.setText(savedText);
-        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/startproject/MessageReceiver.java
@@ -0,0 +1,17 @@
+package com.example.startproject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MessageReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Обнаружено сообщение : " +
+        intent.getStringExtra("\"ru.alexanderklimov.broadcast.Message\""),
+                Toast.LENGTH_SHORT).show();
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
